335bb50272db66f2b07d88447bb8b00f84a76bf0
FER-YASMIN <yasminsenna16730@gmail.com>
2025-03-20 20:19:54 +0000

    novo commit

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -10,30 +10,4 @@ float PIB = 699.28 bilhões de reais;
 int numeros de pontos turisticos = 50;
 
  printf("Desafio super trunfo \n");
- printf("Carta 1 \n");
-
- printf("digite o nome do estado\n");
- scanf("%s", &estado);
-
- printf("digite o codigo da carta \n");
- scanf("%d",&codigo da carta);
-
- prinf("digite o nome da cidade \n");
- scanf("%s", &nome da cidade);
-
- printf("digite a população \n");
- scanf("%d", população);
-
- printf("digite a area em km2 \n");
- scanf("%f",area);
-
- prinf("digite o bip \n");
- scanf("%d", bip);
-
- prinf("digite o numeros de pontos turisticos \n");
- scanf("%d",numeros de pontos turisticos);
-
- return 0;
-
-
-}
+ printd("novo commit \n");
