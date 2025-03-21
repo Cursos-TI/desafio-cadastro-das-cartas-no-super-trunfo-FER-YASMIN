db762fb0037b26fac1bcf0546b4a39fe3ef4fff8
FER-YASMIN <yasminsenna16730@gmail.com>
2025-03-21 19:25:19 +0000

    novo commit

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -11,3 +11,30 @@ int numeros de pontos turisticos = 50;
 
  printf("Desafio super trunfo \n");
  printd("novo commit \n");
+ printf("Carta 1 \n");
+
+ printf("digite o nome do estado\n");
+ scanf("%s", &estado);
+
+ printf("digite o codigo da carta \n");
+ scanf("%d",&codigo da carta);
+
+ prinf("digite o nome da cidade \n");
+ scanf("%s", &nome da cidade);
+
+ printf("digite a população \n");
+ scanf("%d", população);
+
+ printf("digite a area em km2 \n");
+ scanf("%f",area);
+
+ prinf("digite o bip \n");
+ scanf("%d", bip);
+
+ prinf("digite o numeros de pontos turisticos \n");
+ scanf("%d",numeros de pontos turisticos);
+
+ return 0;
+
+
+}
