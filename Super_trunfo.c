621ff28779eb169e2bc29a538681406f4752c9a1
FER-YASMIN <yasminsenna16730@gmail.com>
2025-03-21 21:00:55 +0000

    novo commit

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 
 int main(){
-char estado = 'A';
-char codigo da carta = [A01];
-char nome da cidade = São Paulo
-int população = 12325000;
-float area em km2 = 1521.11 km2;
-float PIB = 699.28 bilhões de reais;
-int numeros de pontos turisticos = 50;
+  // Definindo as variáveis
+  char estado = 'A';
+  char codigo[] = "A01";
+  char nome_da_cidade[] = "São Paulo";
+  int populacao = 12325000;
+  float area = 1521.11;
+  float BIP = 699.28;
+  int num_pontos_turisticos = 50;
 
  printf("Desafio super trunfo \n");
- printd("novo commit \n");
+ printf("novo commit \n");
  printf("Carta 1 \n");
 
  printf("digite o nome do estado\n");
  scanf("%s", &estado);
 
- printf("digite o codigo da carta \n");
- scanf("%d",&codigo da carta);
-
- prinf("digite o nome da cidade \n");
- scanf("%s", &nome da cidade);
-
- printf("digite a população \n");
- scanf("%d", população);
-
- printf("digite a area em km2 \n");
- scanf("%f",area);
-
- prinf("digite o bip \n");
- scanf("%d", bip);
-
- prinf("digite o numeros de pontos turisticos \n");
- scanf("%d",numeros de pontos turisticos);
-
- return 0;
+ 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+ }
